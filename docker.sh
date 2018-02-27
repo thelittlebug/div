1a228c0556f9fc5209f161ca05039c32916941a0
thelittlebug <h@rusznak.at>
2018-02-28 00:00:16 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,8 +9,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y --force-yes docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
 echo "logout/login to get access to docker"
