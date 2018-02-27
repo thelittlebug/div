ce3573c496e561c42462517d16dc817a594f8f2d
thelittlebug <h@rusznak.at>
2018-02-27 23:49:28 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update 
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
